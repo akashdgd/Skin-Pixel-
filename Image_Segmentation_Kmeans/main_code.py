9946b05954b8fb835fb87d487949fd14c1ce5369
akashdgd <36269321+akashdgd@users.noreply.github.com>
2020-02-11 12:09:30 +0530

    Add files via upload

---
+++ Image_Segmentation_Kmeans/main_code.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import cv2
+from sklearn.cluster import KMeans
+import numpy as np
+
+
+im =  cv2.imread('hand1.jpg') #Reads an image into BGR Format
+
+im = cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
+
+all_pixels  = im.reshape((-1,3))
+dominant_colors = 3
+
+km = KMeans(n_clusters=dominant_colors)
+km.fit(all_pixels)
+
+centers = km.cluster_centers_
+centers = np.array(centers,dtype='uint8')
+print(im.shape)
+
+i = 1
+
+#plt.figure(0,figsize=(8,2))
+
+
+colors = []
+
+for each_col in centers:
+    plt.subplot(1,4,i)
+    plt.axis("off")
+    i+=1
+    
+    colors.append(each_col)
+    
+    #Color Swatch
+    a = np.zeros((100,100,3),dtype='uint8')
+    a[:,:,:] = each_col
+    plt.imshow(a)
+    
+plt.show()
+
+# size has to be entered manually
+
+
+new_img = np.zeros((466*466,3),dtype='uint8')
+
+print(new_img.shape)
+for ix in range(new_img.shape[0]):
+    new_img[ix] = 255
+
+# for specific color extraction
+for ix in range(new_img.shape[0]):
+    if km.labels_[ix] == 2:
+        new_img[ix] = colors[km.labels_[ix]]
+    
+    
+new_img = new_img.reshape((im.shape))
+plt.imshow(new_img)
+plt.show()
---
+++ Image_Segmentation_Kmeans/trial.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import cv2
+from sklearn.cluster import KMeans
+import numpy as np
+
+
+im =  cv2.imread('elephant.jpg') #Reads an image into BGR Format
+
+im = cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
+
+
+print(im.shape)
